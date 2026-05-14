ed4986041853c5aebd3a1af268084c02fe602952
Skylar Pay <spay4314@sdsu.edu>
2026-05-13 18:13:10 -0700

    Integrate inventory pickups and organize art assets

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,46 +6,44 @@ public class Item : MonoBehaviour
     [SerializeField] private string itemName;
     [SerializeField] private int amount;
     [SerializeField] private Sprite thisSprite;
+    [SerializeField] private InputActionReference interact;
+
     private bool playerInRange;
     private InventoryManager inventoryManager;
-    [SerializeField] InputActionReference interact;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
         playerInRange = false;
     }
-    
-    private void OnTriggerEnter(Collider collision) 
+
+    private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.CompareTag("Player")) 
+        if (collision.gameObject.CompareTag("Player"))
         {
             playerInRange = true;
         }
     }
 
-    void OnEnable()
+    private void OnTriggerExit(Collider collision)
     {
-        interact.action.Enable();
-    }
-
-    void OnDisable()
-    {
-        interact.action.Disable();
-    }
-    
-    private void OnTriggerExit(Collider collision) 
-    {
-        if (collision.gameObject.CompareTag("Player")) 
+        if (collision.gameObject.CompareTag("Player"))
         {
             playerInRange = false;
         }
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        if (interact != null && !interact.action.enabled)
+        {
+            interact.action.Enable();
+        }
+    }
+
     void Update()
     {
-        if (playerInRange && interact.action.WasPressedThisFrame())
+        if (playerInRange && interact != null && interact.action.WasPressedThisFrame())
         {
             inventoryManager.AddItem(itemName, amount, thisSprite);
             Destroy(gameObject);
--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -1,14 +1,7 @@
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour
 {
-    //SLOT DATA//
     public bool isFull = false;
 
     public GameObject currentItem;
@@ -17,26 +10,17 @@ public class ItemSlot : MonoBehaviour
 
     public void AddItem(string itemName, int amount, Sprite icon)
     {
-        Debug.Log("entered itemSlot Script");
         if (currentItem == null)
         {
-            GameObject item  = Instantiate(draggableItemPrefab, transform);
+            GameObject item = Instantiate(draggableItemPrefab, transform);
             item.transform.SetParent(transform);
+
             item.GetComponent<DraggableItem>().CreateDragItem(itemName, amount, icon);
+
             currentItem = item;
+            isFull = true;
+
             item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
         }
-    }
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
